3fd47ec97351b191813d1da4839a935045924499
Benitta <benittajs2811@gmail.com>
2025-10-23 08:18:07 -0700

    Initial Tourist Helper App using Flask

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ def home():
 @app.route('/api/places')
 def get_places():
     places = [
-        "Kanyakumari",
-        "Courtallam",
-        "Padmanabhapuram Palace",
-        "Meenakshi Amman Temple",
-        "Rameswaram",
-        "Thanjavur Big Temple"
+      {"name": "Kanyakumari", "info": "Southern tip of India, famous for sunrise and Vivekananda Rock."},
+        {"name": "Courtallam", "info": "Known for waterfalls and herbal baths."},
+        {"name": "Padmanabhapuram Palace", "info": "Historic wooden palace near Nagercoil."},
+        {"name": "Meenakshi Amman Temple", "info": "Iconic temple in Madurai with stunning architecture."},
+        {"name": "Rameswaram", "info": "Sacred pilgrimage site with long sea bridge."},
+        {"name": "Thanjavur Big Temple", "info": "UNESCO World Heritage site built by Cholas."}
+
     ]
     return jsonify(places)
 
